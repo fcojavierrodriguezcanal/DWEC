4e2a295c5ed4a0e4b19beb46bdc227247bb9dd55
fcojavierrodriguezcanal <95092710+fcojavierrodriguezcanal@users.noreply.github.com>
2023-01-26 15:47:40 +0100

    cambios

--- Angular/A1/angularUno/src/app/register/register.component.ts
+++ Angular/A1/angularUno/src/app/register/register.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  constructor() { }
+
+  public email = new FormControl('', Validators.required);
+  public password = new FormControl('', Validators.required);
+
+  public newForm = new FormGroup({
+    email: this.email,
+    password: this.password,
+  });
+
+  ngOnInit(): void {
+  }
+
+  public onSubmit(formValue: any){
+    console.log(formValue)
+  }
 
 }
