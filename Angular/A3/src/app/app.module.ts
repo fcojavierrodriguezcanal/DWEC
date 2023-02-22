1bcea84de720833b1d33ec3463e5edd2d19588ef
fcojavierrodriguezcanal <95092710+fcojavierrodriguezcanal@users.noreply.github.com>
2023-02-22 19:17:40 +0100

    comit

--- Angular/A3/src/app/app.module.ts
+++ Angular/A3/src/app/app.module.ts
@@ -2,20 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { LoginComponent } from 'src/app/Components/login/login.component';
-
+import { LoginComponent } from './Components/login/login.component';
+import { ElinicioComponent } from './Components/elinicio/elinicio.component';
+import { ElinicioPageComponent } from './pages/elinicio-page/elinicio-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    ElinicioComponent,
+    ElinicioPageComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule
-    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
